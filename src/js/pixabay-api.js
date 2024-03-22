6efc0b974afc0b2a79f19898b5f2cda939ad9716
MaxsimJSDeveloper <147420858+MaxsimJSDeveloper@users.noreply.github.com>
2024-03-22 16:16:11 +0200

    url

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,8 @@
 import { showLoader } from '../main';
 
 export function fetchPhotoFromPixabay(inputValue) {
+  const BASE_URL = 'https://pixabay.com/api/';
+
   const inputValueForForm = inputValue.trim().split(',').join('+');
   const searchParams = new URLSearchParams({
     key: '42920910-4418ce5fcb505a601cb14954f',
@@ -10,7 +12,10 @@ export function fetchPhotoFromPixabay(inputValue) {
     safesearch: true,
   });
   showLoader();
-  return fetch(`https://pixabay.com/api/?${searchParams}`).then(response => {
+
+  const url = `${BASE_URL}?${searchParams}`;
+
+  return fetch(url).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
